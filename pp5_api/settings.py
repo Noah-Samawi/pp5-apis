4a4ba622177fd72bc8a67a5a0dff4f5a7dfc0890
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-16 13:04:41 +0000

    disable #os.environ['DEV'] on rmv.py for deployment

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '8000-noahsamawi-pp5api-0zqi3177fwg.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
 
 
 # Application definition
