b76940f992f06ed9e983918264f2ef8dd73e1865
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-18 08:14:15 +0000

    changed the default_prileimage

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to="images/", default="../default_profile_12547"
+        upload_to="images/", default="../default_profileimage_qhoc5y"
     )
     facebook_link = models.URLField(blank=True)
     twitter_link = models.URLField(blank=True)
