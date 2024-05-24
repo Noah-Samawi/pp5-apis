2571c8a659886440e8329d15320798d318fc5230
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-05-24 10:53:48 +0000

    fix 404, 500 errors

--- posts/serializers.py
+++ posts/serializers.py
@@ -17,14 +17,21 @@ class PostSerializer(serializers.ModelSerializer):
     countryside_id = serializers.SerializerMethodField()
     comments_count = serializers.ReadOnlyField()
 
-    # Convert creation and update time to human-readable format
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'wanderer_id', 'wanderer_image',
+            'created_at', 'updated_at', 'title', 'content', 'image',
+            'likes_count', 'comments_count', 'like_id', 'countryside_id',
+            'location', 'country'
+        ]
+
     def get_created_at(self, obj):
         return naturaltime(obj.created_at)
 
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
-    # Validate image size and dimensions
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
@@ -34,32 +41,22 @@ class PostSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Image width larger than 4096px!')
         return value
 
-    # Check if the authenticated user is the owner of the post
     def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+        request = self.context.get('request')
+        if request:
+            return request.user == obj.owner
+        return False
 
-    # Get like ID associated with the authenticated user and the post
     def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(owner=user, post=obj).first()
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            like = Like.objects.filter(owner=request.user, post=obj).first()
             return like.id if like else None
         return None
 
-    # Get countryside ID associated with the authenticated user and the post
     def get_countryside_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            countryside = Countryside.objects.filter(owner=user, post=obj).first()
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            countryside = Countryside.objects.filter(owner=request.user, post=obj).first()
             return countryside.id if countryside else None
         return None
-
-    class Meta:
-        model = Post
-        fields = [
-            'id', 'owner', 'is_owner', 'wanderer_id', 'wanderer_image',
-            'created_at', 'updated_at', 'title', 'content', 'image',
-            'likes_count', 'comments_count', 'like_id', 'countryside_id',
-            'location', 'country'
-        ]
